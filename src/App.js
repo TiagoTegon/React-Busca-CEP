be4787447bb2d5e1237ebc6f7f1ce3c9b51c82ff
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-01-09 17:57:40 -0300

    feat: api via cep

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [address, setAddress] = useState(null)
+
+  function buscarEndereco(event) {
+    const cep = event.target.value
+    
+    if(cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => setAddress({
+          cep: cep,
+          street: data.logradouro,
+          district: data.bairro,
+          city: data.localidade,
+          uf: data.uf,
+          ddd: data.ddd
+        }))
+      console.log(address)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Busca CEP</h1>
+        <input onChange={buscarEndereco} />
+        {address &&
+          <>
+            <h2>Resultado da busca</h2>
+            <ul>
+              <li>CEP: {address.cep}</li>
+              <li>Street: {address.street}</li>
+              <li>District: {address.district}</li>
+              <li>City: {address.city}</li>
+              <li>UF: {address.uf}</li>
+              <li>DDD: {address.ddd}</li>
+            </ul>
+          </>
+        }
       </header>
     </div>
   );
